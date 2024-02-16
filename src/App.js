1d8e1be7e7d3b6fa6e9f9739c49bfe3aab89a9c5
Andrés Parra <pipe.parrab@gmail.com>
2024-02-15 19:39:48 -0500

    Añadiendo componente Salary que calcula el salario por hora.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
+import Salary from "./components/Salary/Salary";
 
 function App() {
-
   const [count, setCount] = useState(0);
   /*const [count2, setCount2] = useState(0);
   const [animSpeed, setAnimSpeed] = useState(1);*/
   const [nomSoftware, setNomSoft] = useState("Default");
 
   function handleClick() {
-    setCount(count +1);
+    setCount(count + 1);
     updateProjectName();
   }
 
@@ -60,108 +60,105 @@ function App() {
   }*/
 
   return (
+    <div className="Box-layout">
+      <div className="Section-app App-header">
+        <GeneralForm />
 
-        <div className = "Box-layout">
+        <InputBox
+          id="proyecto"
+          labelText="Nombre del proyecto:"
+          onChange={updateProjectName}
+        />
 
-          <div className = "Section-app App-header">
+        <p>
+          Se ha realizado el costeo de un total de {count} proyectos de
+          software.
+          <br /> Proyecto actual: {count}
+        </p>
+        <MyButton text="Iniciar" onClick={handleClick} />
+        <MyButton text="Reset" onClick={clickReset} />
+      </div>
 
-            <GeneralForm/>
-
-            <InputBox id ="proyecto" labelText = "Nombre del proyecto:" onChange = {updateProjectName}/>
-
-            <p>
-              Se ha realizado el costeo de un total de {count} proyectos de software.
-              <br /> Proyecto actual: {count} 
-            </p>
-            <MyButton text = "Iniciar" onClick = {handleClick}/>      
-            <MyButton text = "Reset" onClick = {clickReset}/>    
-
-          </div>
-             
-          <div className = "Section-report Report-header">
-            
-            <h2>Hello people!</h2>
-            <p id = "Text-report"> Este es un reporte generado en tiempo real y de forma dinámica
-                sobre el costeo del software: {nomSoftware} solicitado por el cliente: y en el cual 
-                discriminamos los siguientes componentes de costo:
-            </p>
-            <h3>Costo de mano de obra</h3>
-            <p>
-              Tabla de estimación del costo de mano de obra.
-            </p>
-          </div>
-
-        </div>
-        
-  );
-}
-
-function MyButton({text, onClick}) {
-  return (
-    /*<div display = "inline">*/
-      <button type = "button" className='Main-button' onClick = {onClick}>
-        {text}  </button>
-    /*</div>*/
-  );
-}
-
-function InputBox({id, labelText, onChange}){
-  return(
-    <div>
-       <label className = "Label"> {labelText} </label>
-       <input id = {id} className ="Input-box" type='text' onInput = {onChange}> 
-       </input>
+      <div className="Section-report Report-header">
+        <h2>Hello people!</h2>
+        <p id="Text-report">
+          {" "}
+          Este es un reporte generado en tiempo real y de forma dinámica sobre
+          el costeo del software: {nomSoftware} solicitado por el cliente: y en
+          el cual discriminamos los siguientes componentes de costo:
+        </p>
+        <h3>Costo de mano de obra</h3>
+        <p>Tabla de estimación del costo de mano de obra.</p>
+        <Salary />
+      </div>
     </div>
   );
 }
 
-function InputText(){
-  return(
-    <input className = "Input-box" type = "text" required />
+function MyButton({ text, onClick }) {
+  return (
+    /*<div display = "inline">*/
+    <button type="button" className="Main-button" onClick={onClick}>
+      {text}{" "}
+    </button>
+    /*</div>*/
   );
 }
 
-function GeneralForm(){
+function InputBox({ id, labelText, onChange }) {
+  return (
+    <div>
+      <label className="Label"> {labelText} </label>
+      <input
+        id={id}
+        className="Input-box"
+        type="text"
+        onInput={onChange}
+      ></input>
+    </div>
+  );
+}
 
-  function addDevps(){
+function InputText() {
+  return <input className="Input-box" type="text" required />;
+}
+
+function GeneralForm() {
+  function addDevps() {
     const $inputNumDevps = document.getElementById("desarrolladores");
-    let num = $inputNumDevps.value
+    let num = $inputNumDevps.value;
     const $devpsTable = document.getElementById("dev-table");
-    let rowCount = $devpsTable.rows.length -1;
+    let rowCount = $devpsTable.rows.length - 1;
     console.log("numero de filas tabla:", rowCount);
     console.log("numero de profesionales", num);
-    
-    if(num > rowCount){
-      for(var i=rowCount; i<num; i++){
 
-        $devpsTable.innerHTML += '<tr>'
-                   + `<td> ${i+1} </td>`
-                   + '<td> <input type = "text"> </td>'
-                   + '<td> <input type = "number" min = "0" step = "10000"> </td>'
-                   + '<td> <input type = "number" min = "0" step = "1"> </td>'
-                + '</tr>'
-        
-        console.log("iteración:", i+1);
-        console.log("filas tabla: ", $devpsTable.rows.length-1);
-  
+    if (num > rowCount) {
+      for (var i = rowCount; i < num; i++) {
+        $devpsTable.innerHTML +=
+          "<tr>" +
+          `<td> ${i + 1} </td>` +
+          '<td> <input type = "text"> </td>' +
+          '<td> <input type = "number" min = "0" step = "10000"> </td>' +
+          '<td> <input type = "number" min = "0" step = "1"> </td>' +
+          "</tr>";
+
+        console.log("iteración:", i + 1);
+        console.log("filas tabla: ", $devpsTable.rows.length - 1);
+
         /*let row = createRow(i+1);
         $devpsTable.appendChild(row);*/
       }
-    }
-    else if(num < rowCount){
-      for(var i=0; i <rowCount-num; i++){
-        $devpsTable.deleteRow(rowCount-i);
+    } else if (num < rowCount) {
+      for (var i = 0; i < rowCount - num; i++) {
+        $devpsTable.deleteRow(rowCount - i);
       }
-      
     }
 
-    rowCount = $devpsTable.rows.length-1;
+    rowCount = $devpsTable.rows.length - 1;
     console.log("numero filas tabla:", rowCount);
-    
   }
 
-  function createRow(index){
-
+  function createRow(index) {
     let row = document.createElement("tr");
     let cell1 = createLabel(index);
     let cell2 = createInput();
@@ -175,8 +172,7 @@ function GeneralForm(){
     return row;
   }
 
-  function createLabel(index){
-
+  function createLabel(index) {
     let cell = document.createElement("td");
     let label = document.createElement("label");
     label.value = index;
@@ -184,46 +180,49 @@ function GeneralForm(){
     return cell;
   }
 
-  function createInput(){
-
+  function createInput() {
     let inp = document.createElement("input");
     inp.type = "text";
     return inp;
   }
 
-  function createNumInput(step){
-
+  function createNumInput(step) {
     let numInp = document.createElement("input");
     numInp.type = "number";
     numInp.min = "0";
     numInp.step = step;
-
   }
 
-  return(
-
+  return (
     <form>
-
       <fieldset>
-        <legend>Información básica del equipo de desarollo asignado al proyecto</legend>
+        <legend>
+          Información básica del equipo de desarollo asignado al proyecto
+        </legend>
         <p>
-          <label for = "desarrolladores"> Número de desarrolladores  </label>
-          <input className = "Input-box"  type = "number" min = "0" max = "1000" 
-                    step = "1" name = "totalEquipo" id="desarrolladores" required />
-          <MyButton text = "Agregar" onClick = {addDevps}/>
+          <label for="desarrolladores"> Número de desarrolladores </label>
+          <input
+            className="Input-box"
+            type="number"
+            min="0"
+            max="1000"
+            step="1"
+            name="totalEquipo"
+            id="desarrolladores"
+            required
+          />
+          <MyButton text="Agregar" onClick={addDevps} />
         </p>
         <br />
-          <DevTable />
+        <DevTable />
       </fieldset>
-
     </form>
   );
 }
 
-function DevTable(){
-
-  return(
-    <table className = "Input-table"  id = "dev-table">
+function DevTable() {
+  return (
+    <table className="Input-table" id="dev-table">
       <tr>
         <th> </th>
         <th> Nombre profesional </th>
